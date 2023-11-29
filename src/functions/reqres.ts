2e890796b88f7049cec236127799497c6c7171c8
Henry Baldwin <henrymbaldwin@gmail.com>
2023-11-29 13:05:16 -0500

    Added types for Query and GetQueryPrompts.

--- src/functions/reqres.ts
+++ src/functions/reqres.ts
@@ -1,15 +1,40 @@
-// reqres.ts
-// types from reqres.rs for use in the addin, these MUST be in parity with reqres.rs
+// Name: reqres.ts
+// Description: types from reqres.rs for use in the addin,
+// Note: these MUST be in parity with reqres.rs
 
-// Login request type
+// login()
 export interface LoginRequest {
   type: "Login";
   username: string;
   password: string;
 }
 
-// Login response type
 export interface LoginResponse {
   type: "Login";
   status: number;
+}
+
+// query()
+export interface QueryRequest {
+  type: "Query";
+  id: string;
+  args: string[];
+}
+
+export interface QueryResponse {
+  type: "Query";
+  status: number;
+  result: string;
+}
+
+//get_query_prompts()
+export interface GetQueryPromptsRequest {
+  type: "GetQueryPrompts";
+  id: string;
+}
+
+export interface GetQueryPromptsResponse {
+  type: "GetQueryPrompts";
+  status: number;
+  prompts: string[];
 }
