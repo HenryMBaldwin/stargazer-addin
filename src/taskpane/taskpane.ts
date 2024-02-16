8995244ed415f80bc414d04740253496fe25e41c
Henry Baldwin <henrymbaldwin@gmail.com>
2024-02-16 17:22:31 -0500

    So thats why it wan't sideloading.

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -8,9 +8,13 @@
 
 // The initialize function must be run each time a new page is loaded
 Office.onReady(() => {
-  //document.getElementById("sideload-msg").style.display = "none";
-  //document.getElementById("app-body").style.display = "flex";
-  //document.getElementById("run").onclick = run;
+  const sideloadMsg = document.getElementById("sideload-msg");
+  const appBody = document.getElementById("app-body");
+  const runButton = document.getElementById("run");
+
+  if (sideloadMsg) sideloadMsg.style.display = "none";
+  if (appBody) appBody.style.display = "flex";
+  if (runButton) runButton.onclick = run;
 });
 
 export async function run() {
